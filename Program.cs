47e2c063aba4fb30356643706d219b54884e0d49
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 23:52:40 -0300

    Finaliza VelocMedia

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dd, dt,  v;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite a distancia percorrida (metros) : ");
+            dd = double.Parse(Console.ReadLine());
+            Console.Write("Digite o tempo gasto (segundos): ");
+            dt = double.Parse(Console.ReadLine());
+
+            v = dd / dt;
+
+            Console.WriteLine($"Distância percorrida : {dd} \nTempo gasto : {dt}");
+            Console.Write($"Velocidade : ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{v:N2} m/s");
+            Console.ResetColor();
+
+        }
+    }
+}
